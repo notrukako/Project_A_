67321c3f2255a71a499a63c7d864677632a99012
notrukako <richard.o.halim@student.uts.edu.au>
2024-04-21 20:35:47 +1000

    new file:   Project/src/In.java 	modified:   Project/src/StreamingSite.java

--- Project/src/StreamingSite.java
+++ Project/src/StreamingSite.java
@@ -2,14 +2,46 @@
 public class StreamingSite {
     public static void main(String[] args) {
 
+
+    
+        System.out.println("-----------------------------------");
+        System.out.println("Streaming Site");
+        System.out.println("-----------------------------------");
+        System.out.println("Select an option:");
+        System.out.println("  1. One Piece");
+        System.out.println("  2. Attack On Titan");
+        System.out.println("  3. The Matrix");
+        System.out.println("  4. Inception");
+        System.out.println("  5. Mata Najwa");
+        System.out.println("  6. Close The Door");
+        System.out.println("  7. Exit");
+
+        int choice = In.nextInt();
+        if (choice == 1) {
+            anime1();
+        } else if (choice == 2) {
+            anime2();
+        } else if (choice == 3) {
+            Movie1();
+        } else if (choice == 2) {
+            Movie2();
+        } else if (choice == 2) {
+            Podcast1();
+        } else if (choice == 2) {
+            Podcast2();
         
+
+        } else {
+            System.out.println("Pick an option 1 - 7");
+        }
+    }
 
 
 
         // Create media items
         Media movie1 = new Movie("The Matrix","Lana Wachowski","1999", 120.0, true,MediaType.MOVIE,MovieGenre.ACTION);
         Media movie2 = new Movie("Inception", "Christopher Nolan","2010", 150.0,true, MediaType.MOVIE,MovieGenre.SCI_FI);
-        Media anime1 = new Anime("Naruto", genre.SHOUNEN, 1,25, "Toei", true, MediaType.ANIME, 25.0);
+        Media anime1 = new Anime("One Piece", genre.SHOUNEN, 1,10, "Toei", true, MediaType.ANIME, 25.0);
         Media anime2 = new Anime("Attack on Titan", genre.ACTION,1, 24, "WIT", true, MediaType.ANIME, 30.0);
         Media podcast1 = new Podcast("Mata Najwa", "Najwa", 20.0, genre.DAILY_LIFE, true, MediaType.PODCAST);
         Media podcast2 = new Podcast("Close The Door", "Deddy Corbuzier", 40.0, genre.COMMENTARY, true, MediaType.PODCAST);
