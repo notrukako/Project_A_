5bcfc6c2cbd296decbf58968f4524d3547aa838c
notrukako <richard.o.halim@student.uts.edu.au>
2024-04-21 20:54:03 +1000

    modified:   Project/src/StreamingSite.java

--- Project/src/StreamingSite.java
+++ Project/src/StreamingSite.java
@@ -18,37 +18,25 @@ public class StreamingSite {
         System.out.println("  8. Exit");
 
         int choice = In.nextInt();
-        if (choice == 1) {
-            anime1();
-        } else if (choice == 2) {
-            anime2();
-        } else if (choice == 3) {
-            Movie1();
-        } else if (choice == 4) {
-            Movie2();
-        } else if (choice == 5) {
-            Podcast1();
-        } else if (choice == 6) {
-            Podcast2();
+        if (choice >= 1 && choice <= 6) {
+            // Create media items
+            Media movie1 = new Movie("The Matrix","Lana Wachowski","1999", 120.0, true,MediaType.MOVIE,MovieGenre.ACTION);
+            Media movie2 = new Movie("Inception", "Christopher Nolan","2010", 150.0,true, MediaType.MOVIE,MovieGenre.SCI_FI);
+            Media anime1 = new Anime("One Piece", genre.SHOUNEN, 1,10, "Toei", true, MediaType.ANIME, 25.0);
+            Media anime2 = new Anime("Attack on Titan", genre.ACTION,1, 24, "WIT", true, MediaType.ANIME, 30.0);
+            Media podcast1 = new Podcast("Mata Najwa", "Najwa", 20.0, genre.DAILY_LIFE, true, MediaType.PODCAST);
+            Media podcast2 = new Podcast("Close The Door", "Deddy Corbuzier", 40.0, genre.COMMENTARY, true, MediaType.PODCAST);
         } else if (choice == 7) {
             String addSubscriber = In.nextLine();
             Subscribers subscriber = new Subscribers(addSubscriber);
         } else if (choice == 8) {
             break;
         } else {
-        System.out.println("Pick an option 1 - 7");
+            System.out.println("Pick an option 1 - 8");
         }
     }
 
 
-
-        // Create media items
-        Media movie1 = new Movie("The Matrix","Lana Wachowski","1999", 120.0, true,MediaType.MOVIE,MovieGenre.ACTION);
-        Media movie2 = new Movie("Inception", "Christopher Nolan","2010", 150.0,true, MediaType.MOVIE,MovieGenre.SCI_FI);
-        Media anime1 = new Anime("One Piece", genre.SHOUNEN, 1,10, "Toei", true, MediaType.ANIME, 25.0);
-        Media anime2 = new Anime("Attack on Titan", genre.ACTION,1, 24, "WIT", true, MediaType.ANIME, 30.0);
-        Media podcast1 = new Podcast("Mata Najwa", "Najwa", 20.0, genre.DAILY_LIFE, true, MediaType.PODCAST);
-        Media podcast2 = new Podcast("Close The Door", "Deddy Corbuzier", 40.0, genre.COMMENTARY, true, MediaType.PODCAST);
 
         subscriber = addSubscriber;
 
